a724ef5ecbd1b07f3a4aeef58201c07e70067dcd
Cristian Ionel <cristian.ionel@gmail.com>
2017-03-29 14:33:07 +0200

    initial commit

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    please log in
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
